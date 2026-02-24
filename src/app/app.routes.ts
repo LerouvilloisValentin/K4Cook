d3547fc5c07a7ca01a6453d21e9269268306a495
LerouvilloisValentin <lerouvillois.valentin61@hotmail.fr>
2026-02-24 13:06:44 +0100

    clean

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { HomeForumComponent } from './home-forum/home-forum.component';
-import { WeatherComponent } from './weather/weather.component';
-import { DetailsSubjectComponent } from './details-subject/details-subject.component';
 import { UserManagementComponent } from './user-management/user-management.component';
-import { QualitairComponent } from './qualitair/qualitair.component';
-import { userAuthGuard } from './auth/user-auth.guard';
 import { adminAuthGuard } from './admin-auth.guard';
 import { SettingComponent } from './setting/setting.component';
 
@@ -14,22 +9,10 @@ export const routes: Routes = [
   { path: '', redirectTo: 'forum', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'forum', component: HomeForumComponent },
-  { path: 'subject/:id', component: DetailsSubjectComponent },
   { path: 'setting', component: SettingComponent },
-  {
-    path: 'weather',
-    component: WeatherComponent,
-    canActivate: [userAuthGuard],
-  },
   {
     path: 'userManagement',
     component: UserManagementComponent,
     canActivate: [adminAuthGuard],
-  },
-  {
-    path: 'airQuality',
-    component: QualitairComponent,
-    canActivate: [userAuthGuard],
   },
 ];
