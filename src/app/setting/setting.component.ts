ce5ccf86c3f79148dc492e7eef85887a41853959
LerouvilloisValentin <lerouvillois.valentin61@hotmail.fr>
2026-03-02 17:10:28 +0100

    use generic modal for delete an user

--- src/app/setting/setting.component.ts
+++ src/app/setting/setting.component.ts
@@ -1,24 +1,35 @@
 import { Component } from '@angular/core';
 import { UserSettingService } from '../user-setting.service';
+import { ModalComponent } from '../shared/components/modal.components';
 
 @Component({
   selector: 'app-setting',
   standalone: true,
-  imports: [],
+  imports: [ModalComponent],
   templateUrl: './setting.component.html',
   styleUrl: './setting.component.css',
 })
 export class SettingComponent {
-  users: any;
+  // users: any;
+  isModalOpen = false;
   constructor(private userSettingService: UserSettingService) {}
 
   onRemoveAccount() {
     const sub = this.userSettingService
       .deleteOwnAccount()
       .subscribe((value: any[]) => {
-        this.users = value;
-        console.log('user', this.users);
+        // this.users = value;
+        // console.log('user', this.users);
+        this.isModalOpen = false;
       });
     console.log('sub', sub);
+  }
+
+  openDeleteModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
   }
 }
