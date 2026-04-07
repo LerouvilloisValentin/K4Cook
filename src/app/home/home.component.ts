fbf10071b71b0b8f948e490f540d1940d5b6c652
LerouvilloisValentin <lerouvillois.valentin61@hotmail.fr>
2026-04-07 13:03:39 +0200

    display a list of user and add a style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserManagementService } from '../user-management.service';
+import { User } from '../user.model';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private userManagementService: UserManagementService) {}
 
+  firstName: String = '';
+  lastName: String = '';
+  arrayOfUsers: User[] = [];
+
+  ngOnInit() {
+    this.userManagementService.getUser().subscribe((user) => {
+      this.arrayOfUsers = user;
+    });
+  }
 }
